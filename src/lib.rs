ff1845daeacf849656d4fdf6ff4e42053fc1540d
Alicja <k2l8m11n2@gmail.com>
2020-06-16 16:58:50 +0200

    Add crate level comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! # urbit-q
+//!
+//! Based on [urbit-ob](https://github.com/urbit/urbit-ob), supports only the `@q` format.
+//!
+//! ## usage
+//!
+//! Note that `encode` pads the beginning to an even number of bytes (as per the
+//! original implementation) and `decode` ignores any dashes or spaces within the
+//! string.
+//! ```rust
+//! let bytes: [u8; 3] = [1, 2, 3];
+//! let string = urbit_q::encode(&bytes); // doznec-binwes
+//! urbit_q::decode(&string).unwrap(); // [0, 1, 2, 3]
+//! urbit_q::decode("doz nec bin wes"); // Some([0, 1, 2, 3])
+//! urbit_q::decode("do-z ne cb inwes"); // Some([0, 1, 2, 3])
+//! urbit_q::decode("hello world"); // None
+//! ```
+
 mod consts;
 
 /// Encodes data to Urbit's `@q` format
